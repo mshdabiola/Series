8f82cceffa13d900d7abae85e2b8050eeda48c1f
mshabiola <mshdabiola@gmal.com>
2023-05-31 14:45:38 +0100

    feat: handle LatexText exeptions

--- common/retex/src/commonMain/kotlin/com/mshdabiola/retex/aimplementation/Latex.kt
+++ common/retex/src/commonMain/kotlin/com/mshdabiola/retex/aimplementation/Latex.kt
@@ -3,12 +3,17 @@ package com.mshdabiola.retex.aimplementation
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -24,6 +29,8 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.dp
 import com.mshdabiola.retex.aimplementation.graphics.ShapeUi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @OptIn(ExperimentalTextApi::class)
 @Composable
@@ -38,9 +45,13 @@ fun LatexText(
     val textMeasurer = rememberTextMeasurer()
     val density = LocalDensity.current.density
 
-    val equations by remember(equationsStr) {
-        mutableStateOf(Formulae.getShapes(density.toDouble(), equationsStr))
+    var equations by remember(equationsStr) {
+        mutableStateOf(emptyList<ShapeUi>())
     }
+    var errorMessage by remember(equationsStr) {
+        mutableStateOf<String?>(null)
+    }
+
     val height by remember {
         derivedStateOf {
             val h = equations.maxOfOrNull {
@@ -58,42 +69,63 @@ fun LatexText(
         }
     }
 
-    Box(modifier = modifier) {
-        Canvas(Modifier.size(width, height)) {
-            scale(scale, pivot = Offset.Zero) {
-                equations.forEach {
-                    if (it is ShapeUi.Text) {
-
-
-                        scale(it.scale, pivot = it.topLeft) {
-                            drawText(
-                                //size= it.size,
-                                textMeasurer = textMeasurer,
-                                text = it.text,
-                                topLeft = it.topLeft,
-                                style = TextStyle.Default.copy(
-                                    fontSize = TextStyle.Default.fontSize,
-                                    fontFamily = FontFamily(getFont(it.fontPath))
-                                )
-                            )
-                        }
-                    }
-                    if (it is ShapeUi.Line) {
-                        drawLine(color = Color.Black, start = Offset.Zero, end = Offset(200f, 200f))
-                    }
-                    if (it is ShapeUi.Rectangle) {
-
-                        drawRect(
-                            Color.Black,
-                            topLeft = it.topLeft,
-                            size = it.size,
-                            style = if (it.isFill) Fill else Stroke(0.4f)
-                        )
-                        //drawLine(color = Color.Black, start = Offset.Zero, end = Offset(200f,200f))
-                    }
-                }
+    LaunchedEffect(equations) {
+        withContext(Dispatchers.Default) {
+            try {
+                errorMessage = null
+                equations = Formulae.getShapes(density.toDouble(), equationsStr)
+            } catch (e: Exception) {
+                errorMessage = e.message
             }
 
         }
+    }
+
+    Box(modifier = modifier.fillMaxWidth()) {
+        if (errorMessage == null) {
+            Canvas(Modifier.size(width, height).align(Alignment.Center)) {
+                scale(scale, pivot = Offset.Zero) {
+                    equations.forEach {
+                        if (it is ShapeUi.Text) {
+
+
+                            scale(it.scale, pivot = it.topLeft) {
+                                drawText(
+                                    //size= it.size,
+                                    textMeasurer = textMeasurer,
+                                    text = it.text,
+                                    topLeft = it.topLeft,
+                                    style = TextStyle.Default.copy(
+                                        fontSize = TextStyle.Default.fontSize,
+                                        fontFamily = FontFamily(getFont(it.fontPath))
+                                    )
+                                )
+                            }
+                        }
+                        if (it is ShapeUi.Line) {
+                            drawLine(
+                                color = Color.Black,
+                                start = Offset.Zero,
+                                end = Offset(200f, 200f)
+                            )
+                        }
+                        if (it is ShapeUi.Rectangle) {
+
+                            drawRect(
+                                Color.Black,
+                                topLeft = it.topLeft,
+                                size = it.size,
+                                style = if (it.isFill) Fill else Stroke(0.4f)
+                            )
+                            //drawLine(color = Color.Black, start = Offset.Zero, end = Offset(200f,200f))
+                        }
+                    }
+                }
+
+            }
+        } else {
+            Text("Error: $errorMessage",modifier=Modifier.fillMaxWidth())
+        }
+
     }
 }
