14539afa9693f249942a9bca3c3f18a17532d18c
mshabiola <mshdabiola@gmal.com>
2023-08-20 21:40:45 +0100

    feat(general): add question modules

---
+++ modules/navigationquestion/build.gradle.kts
@@ -0,0 +1,21 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    id("mshdabiola.mpp.library")
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.mshdabiola.navigationquestion"
+}
+
+kotlin {
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.decompose.core)
+                //implementation(libs.decompose.android)
+                implementation(libs.decompose.compose.jetbrains)
+            }
+        }
+    }
+}
