c3073701a252147465223bc31118fb92348c83f8
mshabiola <mshdabiola@gmal.com>
2023-11-27 21:39:58 +0100

    feat(common): fix minor bugs

--- modules/ui/editor/src/commonMain/kotlin/com/mshdabiola/series/CommonScreen.kt
+++ modules/ui/editor/src/commonMain/kotlin/com/mshdabiola/series/CommonScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -62,7 +61,8 @@ fun CommonScreen(
             if (show) {
                 ModalBottomSheet(
                     onDismissRequest = { show = false },
-                    Modifier.navigationBarsPadding()
+                    Modifier.fillMaxSize(),
+                    sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
                 ) {
                     secondScreen()
                 }
