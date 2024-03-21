6acf59c8cfe388805b58fdf9bd403ae39d9fad6b
mshdabiola <mshdabiola@gmail.com>
2024-03-22 00:43:35 +0100

    fix bug

--- modules/ui/cbt/src/main/java/com/mshdabiola/ui/Resourse.android.kt
+++ modules/ui/cbt/src/main/java/com/mshdabiola/ui/Resourse.android.kt
@@ -9,35 +9,35 @@ import com.mshdabiola.ui.cbt.R
 
 @Composable
 actual fun getStringSubject(): String {
-    return stringResource(id = R.string.subject)
+    return stringResource(id = R.string.modules_ui_cbt_subject)
 }
 
 @Composable
 actual fun getStringType(): String {
-    return stringResource(id = R.string.type)
+    return stringResource(id = R.string.modules_ui_cbt_type)
 }
 
 @Composable
 actual fun getExamPart(): Array<String> {
-    return stringArrayResource(id = R.array.exam_part)
+    return stringArrayResource(id = R.array.modules_ui_cbt_exam_part)
 }
 
 @Composable
 actual fun getIconLayer2(): Painter {
-    return painterResource(id = R.drawable.layer_2)
+    return painterResource(id = R.drawable.modules_ui_cbt_layer_2)
 }
 
 @Composable
 actual fun getIconLayer1(): Painter {
-    return painterResource(id = R.drawable.layer_1)
+    return painterResource(id = R.drawable.modules_ui_cbt_layer_1)
 }
 
 @Composable
 actual fun getIconLayer3(): Painter {
-    return painterResource(id = R.drawable.layer__1)
+    return painterResource(id = R.drawable.modules_ui_cbt_layer__1)
 }
 
 @Composable
 actual fun getSection(): Array<String> {
-    return stringArrayResource(id = R.array.sections)
+    return stringArrayResource(id = R.array.modules_ui_cbt_sections)
 }
