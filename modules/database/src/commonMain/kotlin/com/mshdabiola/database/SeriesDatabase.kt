a8a445d382782a92b2b9fa2913263f8279c30a2f
mshdabiola <mshdabiola@gmail.com>
2024-07-13 01:46:30 +0100

    fix(database): update user entity and database version
    This commit updates the user entity to include a 'type' field and resets the database version to 2
    .
    
    - Added a 'type' field to the `UserEntity` to differentiate between user types.
    - Reset the database version to 2 to reflect the schema changes.
    - Removed unnecessary auto migrations.

--- modules/database/src/commonMain/kotlin/com/mshdabiola/database/SeriesDatabase.kt
+++ modules/database/src/commonMain/kotlin/com/mshdabiola/database/SeriesDatabase.kt
@@ -36,11 +36,11 @@ import com.mshdabiola.database.model.topic.TopicEntity
         SubjectEntity::class,
         TopicEntity::class,
     ],
-    version = 4,
+    version = 2,
     autoMigrations = [
         AutoMigration(from = 1, to = 2, spec = DatabaseMigrations.Schema1to2::class),
-        AutoMigration(from = 2, to = 3, spec = DatabaseMigrations.Schema2to3::class),
-        AutoMigration(from = 3, to = 4, spec = DatabaseMigrations.Schema2to3::class),
+//        AutoMigration(from = 2, to = 3, spec = DatabaseMigrations.Schema2to3::class),
+//        AutoMigration(from = 3, to = 4, spec = DatabaseMigrations.Schema2to3::class),
 
 
     ],
