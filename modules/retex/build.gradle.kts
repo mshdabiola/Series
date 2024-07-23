4f49e28073c8c1d0c93d09eafd9a6edcbc50e2a5
mshdabiola <mshdabiola@gmail.com>
2024-07-24 00:59:54 +0100

    feat(retex): add compose material icons extended and components resources
    This commit adds dependencies on `compose.materialIconsExtended` and `compose.components.
    resources` to the retex module. This will allow the use of extended Material icons and component resources in the retex module.

--- modules/retex/build.gradle.kts
+++ modules/retex/build.gradle.kts
@@ -18,6 +18,8 @@ kotlin {
                 implementation(compose.runtime)
                 implementation(compose.foundation)
                 implementation(compose.material3)
+                implementation(compose.materialIconsExtended)
+                implementation(compose.components.resources)
             }
         }
 
