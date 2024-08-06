3ceefe27e6b494b6914ab7b6643eef19ca677127
mshdabiola <mshdabiola@gmail.com>
2024-08-06 11:47:12 +0100

    feat: Configure Maven publishing for modules
    This commit configures Maven publishing for the following modules:
    
    - retex
    - jretex
    - database
    
    - testing
    - model
    
    Each module's `build.gradle.kts` file is updated to include the necessary plugins and configurations for publishing to a GitHub Packages repository. The POM metadata is also configured to provide information about the published artifacts.

--- modules/jretex/build.gradle.kts
+++ modules/jretex/build.gradle.kts
@@ -3,45 +3,94 @@ import java.util.Properties
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
-    id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.29.0"
+
 }
-group = "com.mshdabiola.series"
-version = libs.versions.versionName
-
-var project : Properties?=null
-try {
-    project=  File(rootDir, "local.properties").inputStream().use {
-        Properties().apply { load(it) }
-
-    }
-    // println("user ${ project?.getProperty("gpr.password")}")
-
-
-} catch (e: Exception) {
-
-    //e.printStackTrace()
-}
-
-publishing{
-    publications {
-        create<MavenPublication>("maven"){
-            groupId="com.mshdabiola.series"
-            artifactId="jretex"
-            version= libs.versions.versionName.get()
-            from(components["java"])
-        }
-    }
+publishing {
     repositories {
         maven {
-            name = "library"
+            name = "githubPackages"
             url = uri("https://maven.pkg.github.com/mshdabiola/series")
-            credentials {
-                username = project?.getProperty("gpr.userid")  ?: System.getenv("USERID")
-                password = project?.getProperty("gpr.password") ?: System.getenv("PASSWORD")
-            }
+            credentials(PasswordCredentials::class)
+
         }
     }
 }
+
+mavenPublishing {
+    // Define coordinates for the published artifact
+    coordinates(
+        groupId =  libs.versions.groupId.get(),
+        artifactId = "jretex",
+        version = libs.versions.versionName.get()
+    )
+
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("J retext KMP Library")
+        description.set("Sample Kotlin MultiPlatform Library Test")
+        inceptionYear.set("2024")
+        url.set("https://github.com/mshdabiola/series")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("mshdabiola")
+                name.set("Lawal abiola")
+                email.set("mshdabiola@gmail.com")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/mshdabiola/series")
+        }
+    }
+}
+//group = "com.mshdabiola.series"
+//version = libs.versions.versionName
+
+var project : Properties?=null
+//try {
+//    project=  File(rootDir, "local.properties").inputStream().use {
+//        Properties().apply { load(it) }
+//
+//    }
+//    // println("user ${ project?.getProperty("gpr.password")}")
+//
+//
+//} catch (e: Exception) {
+//
+//    //e.printStackTrace()
+//}
+
+//publishing{
+//    publications {
+//        create<MavenPublication>("maven"){
+//            groupId="com.mshdabiola.series"
+//            artifactId="jretex"
+//            version= libs.versions.versionName.get()
+//            from(components["java"])
+//        }
+//    }
+//    repositories {
+//        maven {
+//            name = "library"
+//            url = uri("https://maven.pkg.github.com/mshdabiola/series")
+//            credentials {
+//                username = project?.getProperty("gpr.userid")  ?: System.getenv("USERID")
+//                password = project?.getProperty("gpr.password") ?: System.getenv("PASSWORD")
+//            }
+//        }
+//    }
+//}
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
