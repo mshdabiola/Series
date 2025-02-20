0dddff7d4b5f033e7087ef10256a3962c1e943b7
mshdabiola <mshdabiola@gmail.com>
2025-02-20 23:58:57 +0100

    feat: Replace `maven-publish` with custom plugin
    This commit replaces the `com.vanniktech.maven.publish` plugin with the custom `mshdabiola.android.library.publish` plugin in the following modules:
    
    -   jretex
    -   database
    -   testing
    -   model
    
    It removes redundant configurations related to publishing, such as repositories, pom, developers,licenses, SCM information, and central repository setup, as this functionality will be handled by the custom plugin.
    It also removes the group id and version form mavenPublishing coordinates, becuse the custom plugin manage it.
    It also remove the use of `PasswordCredentials`.

--- modules/database/build.gradle.kts
+++ modules/database/build.gradle.kts
@@ -4,63 +4,20 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 plugins {
     id("mshdabiola.android.library")
     id("mshdabiola.android.room")
-    id("com.vanniktech.maven.publish") version "0.29.0"
+    id("mshdabiola.android.library.publish")
 
 }
-publishing {
-    repositories {
-        maven {
-            name = "githubPackages"
-            url = uri("https://maven.pkg.github.com/mshdabiola/series")
-            credentials(PasswordCredentials::class)
-
-        }
-    }
-}
-
 mavenPublishing {
     // Define coordinates for the published artifact
     coordinates(
-        groupId =  libs.versions.groupId.get(),
         artifactId = "seriesdatabase",
-        version = libs.versions.versionName.get()
     )
 
     // Configure POM metadata for the published artifact
     pom {
         name.set("Series Database")
         description.set("Database for Series")
-        inceptionYear.set("2024")
-        url.set("https://github.com/mshdabiola/series")
-
-        licenses {
-            license {
-                name.set("MIT")
-                url.set("https://opensource.org/licenses/MIT")
-            }
-        }
-
-        // Specify developers information
-        developers {
-            developer {
-                id.set("mshdabiola")
-                name.set("Lawal abiola")
-                email.set("mshdabiola@gmail.com")
-            }
-        }
-
-        // Specify SCM information
-        scm {
-            url.set("https://github.com/mshdabiola/series")
-        }
     }
-
-
-    // Configure publishing to Maven Central
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-
-    // Enable GPG signing for all publications
-    signAllPublications()
 }
 android {
     namespace = "com.mshdabiola.seriesdatabase"
--- modules/model/build.gradle.kts
+++ modules/model/build.gradle.kts
@@ -3,63 +3,22 @@ import com.vanniktech.maven.publish.SonatypeHost
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("mshdabiola.android.library")
+    id("mshdabiola.android.library.publish")
     alias(libs.plugins.kotlin.serialization)
-    id("com.vanniktech.maven.publish") version "0.29.0"
-
 }
-publishing {
-    repositories {
-        maven {
-            name = "githubPackages"
-            url = uri("https://maven.pkg.github.com/mshdabiola/series")
-            credentials(PasswordCredentials::class)
 
-        }
-    }
-}
 
 mavenPublishing {
     // Define coordinates for the published artifact
     coordinates(
-        groupId = libs.versions.groupId.get(),
         artifactId = "seriesmodel",
-        version = libs.versions.versionName.get()
     )
 
     // Configure POM metadata for the published artifact
     pom {
         name.set("Series Model")
         description.set("Model for Series")
-        inceptionYear.set("2024")
-        url.set("https://github.com/mshdabiola/series")
-
-        licenses {
-            license {
-                name.set("MIT")
-                url.set("https://opensource.org/licenses/MIT")
-            }
-        }
-
-        // Specify developers information
-        developers {
-            developer {
-                id.set("mshdabiola")
-                name.set("Lawal abiola")
-                email.set("mshdabiola@gmail.com")
-            }
-        }
-
-        // Specify SCM information
-        scm {
-            url.set("https://github.com/mshdabiola/series")
-        }
     }
-
-    // Configure publishing to Maven Central
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-
-    // Enable GPG signing for all publications
-    signAllPublications()
 }
 kotlin {
     sourceSets {
