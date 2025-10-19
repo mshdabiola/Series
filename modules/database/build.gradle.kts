462137d67d47ce71b5507d727e387137cf3490cb
mshdabiola <mshdabiola@gmail.com>
2025-10-19 15:59:22 +0100

    refactor: Standardize license headers and enable ABI validation
    This commit standardizes the project's code by updating and adding Apache 2.0 license headers across numerous files in the `database` module. This includes DAOs, entities, and relation classes.
    
    Additionally, ABI (Application Binary Interface) validation has been enabled for the `database` module in its `build.gradle.kts` file. This will help ensure binary compatibility across future versions of the module. API dump files for `android`, `jvm`, and `klib` targets have been generated as part of this process.

--- modules/database/build.gradle.kts
+++ modules/database/build.gradle.kts
@@ -91,6 +91,11 @@ kotlin {
             }
         }
     }
+    @OptIn(org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation::class)
+    abiValidation {
+        // Use the set() function to ensure compatibility with older Gradle versions
+        enabled.set(true)
+    }
     sourceSets {
         all {
             languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
--- modules/database/src/nonJsMain/kotlin/com/mshdabiola/database/di/Platform.kt
+++ modules/database/src/nonJsMain/kotlin/com/mshdabiola/database/di/Platform.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,7 +17,6 @@ package com.mshdabiola.database.di
 
 import com.mshdabiola.database.SeriesDatabase
 import org.koin.core.module.Module
-import org.koin.core.qualifier.qualifier
 import org.koin.dsl.module
 
 expect val databaseModule: Module
